c28ec349e54e6f17d23e384a1f0cbb47cc3bb123
Roland Persson <roland.persson@bontouch.com>
2013-09-04 14:32:50 +0200

    Added static library version for mac.

---
+++ xcode/OpenCV-MacOSXStatic/opencv2/cvconfig.h
@@ -0,0 +1,17 @@
+//
+//  cvconfig.h
+//  OpenCV-iOS
+//
+//  Created by Roland Persson on 3/10/13.
+//  Copyright (c) 2013 OpenCV. All rights reserved.
+//
+
+#ifndef OpenCV_iOS_cvconfig_h
+#define OpenCV_iOS_cvconfig_h
+
+#define HAVE_JPEG
+#define HAVE_PNG
+#define HAVE_COCOA
+
+
+#endif
